0df11d0a158f09bc86644ea74aa0743688cc677f
Muhajirin Ilyas <muhajirinlpu@pm.me>
2022-03-17 22:44:38 +0700

    add test

--- tests/Feature/DecisionTreeTest.php
+++ tests/Feature/DecisionTreeTest.php
@@ -1,7 +1,13 @@
 <?php
 
-it('decision tree test', function () {
+it('decision tree test signal', function () {
     $this->artisan('demo:decision-tree', [
         'dataset' => 'data/signal.csv',
+    ]);
+});
+
+it('decision tree test heart_decease', function () {
+    $this->artisan('demo:decision-tree', [
+        'dataset' => 'data/heart_decease.csv',
     ]);
 });
